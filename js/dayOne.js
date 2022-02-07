af5f100676fb7ffd5dbe971f8fbf582927f50195
William MacInnis <wmacinnis@bayviewglen.ca>
2022-02-07 13:08:52 -0500

    Js day 1

---
+++ js/dayOne.js
@@ -0,0 +1,14 @@
+// Declare Variables
+let x = 7;
+const names = ['Jaqueline', 'Amy', 'Sunil'];
+
+// Let and const are scope sensitive
+
+//Var Names
+/**
+ * number
+ * object
+ */
+let name = "William";
+let isFinished = false;
+console.log(3/2);
